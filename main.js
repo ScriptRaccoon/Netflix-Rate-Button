3357e9a88a4909cf465fbbc5295b595ca73f034f
Script Raccoon <54458975+ScriptRaccoon@users.noreply.github.com>
2021-09-28 20:52:50 +0200

    small adjustments

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const currentMovie = {
     rating: null,
 };
 
-const ratingOptions = ["up", "down"];
-
 let ratingPossible = false;
 
 $(".ratingImage").click(showRatingOptions);
@@ -26,27 +24,22 @@ function hideRatingOptions() {
 }
 
 for (let i = 0; i < 2; i++) {
-    const option = ratingOptions[i];
-    const option2 = ratingOptions[1 - i];
+    const option = i == 0 ? "up" : "down";
+    const option2 = i == 0 ? "down" : "up";
     $(`.thumbs-${option}`).click(function () {
         if (!ratingPossible) return;
         ratingPossible = false;
-        $(this).addClass("flashing");
-        setTimeout(() => {
-            $(this).removeClass("flashing");
-        }, 80);
+        $(this).flashClass("flash", 80);
         if (currentMovie.rating != option) {
             currentMovie.rating = option;
-            $(".ratingLabel").text("Rated");
             $(`.thumbs-${option}-image`).attr("src", `/img/thumbs-${option}-filled.svg`);
             $(`.thumbs-${option2}-image`).attr("src", `/img/thumbs-${option2}.svg`);
             setTimeout(() => {
                 hideRatingOptions();
                 $(".ratingImage").attr("src", `/img/thumbs-${option}-filled.svg`);
             }, 1000);
-        } else if (currentMovie.rating == option) {
+        } else {
             currentMovie.rating = null;
-            $(".ratingLabel").text("Rate");
             $(`.thumbs-${option}-image`).attr("src", `/img/thumbs-${option}.svg`);
             setTimeout(() => {
                 hideRatingOptions();
@@ -55,3 +48,10 @@ for (let i = 0; i < 2; i++) {
         }
     });
 }
+
+$.fn.flashClass = function (className, delay) {
+    this.addClass(className);
+    setTimeout(() => {
+        this.removeClass(className);
+    }, delay);
+};
