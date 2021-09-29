93960e5f221ab0159f874c869677cb0d7fe201f5
Script Raccoon <54458975+ScriptRaccoon@users.noreply.github.com>
2021-09-29 07:34:58 +0200

    toggle add function

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const currentMovie = {
     id: "28630857",
     title: "Princess Mononoke",
     rating: null,
+    added: false,
 };
 
 let ratingPossible = false;
 
 $(".ratingImage").click(showRatingOptions);
-$(".cancel").click(hideRatingOptions);
+
+$(".addImage").click(toggleAdd);
+
+$(".cancel").click(() => {
+    if (!ratingPossible) return;
+    hideRatingOptions();
+});
 
 function showRatingOptions() {
     ratingPossible = true;
@@ -23,6 +30,18 @@ function hideRatingOptions() {
     $(".ratingControl").removeClass("out");
 }
 
+function toggleAdd() {
+    if (currentMovie.added) {
+        currentMovie.added = false;
+        $(".addLabel").text("Add");
+        $(".addImage").attr("src", "/img/plus.svg");
+    } else {
+        currentMovie.added = true;
+        $(".addLabel").text("Added");
+        $(".addImage").attr("src", "/img/check.svg");
+    }
+}
+
 for (let i = 0; i < 2; i++) {
     const option = i == 0 ? "up" : "down";
     const option2 = i == 0 ? "down" : "up";
@@ -31,20 +50,22 @@ for (let i = 0; i < 2; i++) {
         ratingPossible = false;
         $(this).flashClass("flash", 80);
         if (currentMovie.rating != option) {
+            $(".ratingLabel").text("Rated");
             currentMovie.rating = option;
             $(`.thumbs-${option}-image`).attr("src", `/img/thumbs-${option}-filled.svg`);
             $(`.thumbs-${option2}-image`).attr("src", `/img/thumbs-${option2}.svg`);
             setTimeout(() => {
                 hideRatingOptions();
                 $(".ratingImage").attr("src", `/img/thumbs-${option}-filled.svg`);
-            }, 1000);
+            }, 600);
         } else {
+            $(".ratingLabel").text("Rate");
             currentMovie.rating = null;
             $(`.thumbs-${option}-image`).attr("src", `/img/thumbs-${option}.svg`);
             setTimeout(() => {
                 hideRatingOptions();
                 $(".ratingImage").attr("src", "/img/thumbs-up.svg");
-            }, 1000);
+            }, 600);
         }
     });
 }
